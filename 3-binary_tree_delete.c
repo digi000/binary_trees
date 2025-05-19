d0e891a323b94c8c684e44e4e07a628d13228a61
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-05-19 21:32:58 +0200

    3t-

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,19 +1,14 @@
 #include "binary_trees.h"
 
-void intr(binary_tree_t *tree);
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to the root node of the tree to delete
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
-
-	if (tree)
-	{
-		intr(tree);
-	}
-}
-
-void intr(binary_tree_t *root)
-{
-	if (root == NULL) return;
-	intr(root->left);
-	free(root->left);
-	intr(root->right);
+    if (tree == NULL)
+        return;
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+    free(tree);
 }
